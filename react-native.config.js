0e55586be0bb68461860a3e9ec76c3295f6e13d4
Yann Leflour <yannl@bam.tech>
2019-08-28 11:14:39 +0200

    :wrench: (npm) move to @bam.tech/react-native-make

--- react-native.config.js
+++ react-native.config.js
@@ -2,12 +2,11 @@
 const { resolve } = require('path');
 const { lstatSync } = require('fs');
 
-const res = lstatSync(resolve('.', 'node_modules', 'react-native-make'));
-
 /**
  * For faster development workflow, when npm linked
  * we want this library to be transpiled at runtime using the ts-node resolver
  */
+const res = lstatSync(resolve('.', 'node_modules', '@bam.tech', 'react-native-make'));
 if (res.isSymbolicLink()) {
   console.warn('Detected linked install of react-native-make, compiling at runtime...');
   require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
