f2e8f9c682b9cc1c44d395ff1618fe4636f24a6a
Yann Leflour <yannl@bam.tech>
2019-08-28 10:52:22 +0200

    :sparkles: (npm) prepare for publication

--- react-native.config.js
+++ react-native.config.js
@@ -4,15 +4,16 @@ const { lstatSync } = require('fs');
 
 const res = lstatSync(resolve('.', 'node_modules', 'react-native-make'));
 
+/**
+ * For faster development workflow, when npm linked
+ * we want this library to be transpiled at runtime using the ts-node resolver
+ */
 if (res.isSymbolicLink()) {
   console.warn('Detected linked install of react-native-make, compiling at runtime...');
   require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
-  const { rnPluginConfig } = require('./rn-plugin.config');
+  const { rnPluginConfig } = require('./src/rn-plugin.config');
 
   module.exports = rnPluginConfig;
 } else {
-  require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
-  const { rnPluginConfig } = require('./rn-plugin.config');
-
-  module.exports = rnPluginConfig;
+  module.exports = require('./dist/rn-plugin.config');
 }
