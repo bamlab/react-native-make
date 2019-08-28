139e5e971d65468fa687f9d238a5e67ef6ad1840
Yann Leflour <yannl@bam.tech>
2019-08-28 10:41:24 +0200

    :sparkles: (plugin) add runtime env detection

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 const { resolve } = require('path');
-require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
-const { rnPluginConfig } = require('./rn-plugin.config');
+const { lstatSync } = require('fs');
 
-module.exports = rnPluginConfig;
+const res = lstatSync(resolve('.', 'node_modules', 'react-native-make'));
+
+if (res.isSymbolicLink()) {
+  console.warn('Detected linked install of react-native-make, compiling at runtime...');
+  require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
+  const { rnPluginConfig } = require('./rn-plugin.config');
+
+  module.exports = rnPluginConfig;
+} else {
+  require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
+  const { rnPluginConfig } = require('./rn-plugin.config');
+
+  module.exports = rnPluginConfig;
+}
