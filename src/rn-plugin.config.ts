8ec84395a1839f2ba8c2c527e2e13d16b65c0834
Yann Leflour <yannl@bam.tech>
2020-02-28 14:59:47 +0100

    fix: remove rn-cli  v1 compatibility layer

--- src/rn-plugin.config.ts
+++ src/rn-plugin.config.ts
@@ -1,28 +1,9 @@
 import { UserDependencyConfig } from '@react-native-community/cli';
 import 'colors';
 import { setIconCommand, setSplashScreenCommand } from './modules';
-import { resolve } from 'path';
-import { createBackwardCompatibleConfig } from './rn-compatibility-layer';
-
-function getRNCliPackageFile() {
-  try {
-    // Yarn module resolution
-    return require(resolve('.', 'node_modules/@react-native-community/cli/package.json'));
-  } catch {
-    // NPM module resolution
-    return require(resolve(
-      '.',
-      'node_modules/react-native/node_modules/@react-native-community/cli/package.json'
-    ));
-  }
-}
 
 const v2Config: UserDependencyConfig = {
   commands: [setIconCommand, setSplashScreenCommand],
 };
 
-const rnCliVersion = getRNCliPackageFile();
-
-const isUsingV1 = !!rnCliVersion.version.match(/^[0-1]\./);
-
-export const rnPluginConfig: any = isUsingV1 ? createBackwardCompatibleConfig(v2Config) : v2Config;
+export const rnPluginConfig: any = v2Config;
